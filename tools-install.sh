43a1d4de944d8fe4e3843c173609c003e019802b
Jack <fallensnow.qing@gmail.com>
2018-11-12 21:00:26 +1100

    update
    1

--- tools-install.sh
+++ tools-install.sh
@@ -11,7 +11,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo apt-get install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-suod apt-get install sublime-text -y
+sudo apt-get install sublime-text -y
 
 
 # Install subfinder

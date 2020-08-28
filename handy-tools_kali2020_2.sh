2b34b32bfeca693fe02fdb420e906004cc71ccc6
Jack <fallensnow.qing@gmail.com>
2020-08-28 14:31:44 +1000

    Update handy-tools_kali2020_2.sh
    Update handy-tools_kali2020_2.sh to handy-tools_kali2020_3

--- handy-tools_kali2020_2.sh
+++ handy-tools_kali2020_2.sh
@@ -9,6 +9,8 @@ sudo apt-get full-upgrade -y
 sudo apt-get install -y python3-pip python-pip golang 
 sudo apt-get install -y testssl.sh seclists neofetch plank lolcat gnome-terminal apt-transport-https
 sudo apt-get install -y kali-community-wallpapers
+sudo apt-get install -y libssl-dev libffi-dev python-dev build-essential
+sudo pip install wheel
 
 # Install sublime-text3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -17,6 +19,7 @@ sudo apt-get update
 sudo apt-get install sublime-text
 
 # Install VS code
+###! update this link before run this script
 #wget https://az764295.vo.msecnd.net/stable/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/code_1.45.1-1589445302_amd64.deb
 sudo apt install /root/hivint/tmp/hivint-kali-backup/code_1.45.1-1589445302_amd64.deb
 
@@ -32,6 +35,7 @@ go get github.com/ffuf/ffuf
 go get github.com/cjbassi/gotop
 go get -u github.com/tomnomnom/gron
 go get -u github.com/tomnomnom/httprobe
+go get -u github.com/jaeles-project/gospider
 
 # Install evil-winrm
 gem install evil-winrm
@@ -43,20 +47,37 @@ chmod +x /usr/local/bin/gosecretsdump
 
 cd /opt/
 
+# Install RustScan
+###! update the URL before run this script
+wget https://github.com/RustScan/RustScan/releases/download/1.7.1/rustscan_1.7.1_amd64.deb
+dpkg -i rustscan_1.7.1_amd64.deb
+rm rustscan_1.7.1_amd64.deb
+
+# Install gp3finder
+git clone https://bitbucket.org/grimhacker/gpppfinder.git
+cd gpppfinder
+python3 setup.py install
+cd /opt/
+
 # Install latest smbmap
 git clone https://github.com/ShawnDEvans/smbmap.git
 cd /opt/smbmap/
-python3 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 cd /opt/
 
 # Install latest Crackmapexec
-wget https://github.com/byt3bl33d3r/CrackMapExec/releases/download/v5.0.2dev/cme-ubuntu-latest.zip
-unzip cme-ubuntu-latest.zip
-chmod +x cme
-mv cme /usr/local/bin/
-rm cme-ubuntu-latest.zip
+#wget https://github.com/byt3bl33d3r/CrackMapExec/releases/download/v5.0.2dev/cme-ubuntu-latest.zip
+#unzip cme-ubuntu-latest.zip
+#chmod +x cme
+#mv cme /usr/local/bin/
+#rm cme-ubuntu-latest.zip
+git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
+cd CrackMapExec
+python3 setup.py install
+cd /opt/
 
 # Install latest aquatone
+###! update this before run the script
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
 unzip aquatone_linux_amd64_1.7.0.zip aquatone
 mv aquatone /usr/local/bin/
@@ -125,6 +146,7 @@ wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh -O /
 wget https://raw.githubusercontent.com/sleventyeleven/linuxprivchecker/master/linuxprivchecker.py -O /var/www/html/privesc/linuxprivchecker.py
 wget https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh -O /var/www/html/privesc/linux-exploit-suggester.sh
 wget https://raw.githubusercontent.com/jondonas/linux-exploit-suggester-2/master/linux-exploit-suggester-2.pl -O /var/www/html/privesc/linux-exploit-suggester-2.pl
+wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh -O /var/www/html/privesc/linpeas.sh
 
 
 # Create and setup windows web folder
@@ -139,11 +161,12 @@ wget https://raw.githubusercontent.com/rasta-mouse/Sherlock/master/Sherlock.ps1 
 # Download Rubeus.exe
 wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe -O /var/www/html/windows/Rubeus.exe
 # Download Grouper2.exe
-wget https://github.com/l0ss/Grouper2/releases/download/0.9.62/Grouper2.exe -O /var/www/html/windows/Grouper2.exe
+wget https://github.com/l0ss/Grouper2/releases/download/0.9.64/Grouper2.exe -O /var/www/html/windows/Grouper2.exe
 # Download gp3finder.exe
 wget https://bitbucket.org/grimhacker/gpppfinder/downloads/gp3finder_v4.0.exe -O /var/www/html/windows/gp3finder.exe
 # Download WindowsEnum.ps1
 wget https://raw.githubusercontent.com/absolomb/WindowsEnum/master/WindowsEnum.ps1 -O /var/www/html/windows/WindowsEnum.ps1
+wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/winPEAS/winPEASbat/winPEAS.bat -O /var/www/html/windows/winPEAS.bat
 # Download the latest PowerView.ps1
 wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/dev/Recon/PowerView.ps1 -O /var/www/html/windows/PowerView.ps1
 # Download SharpHound

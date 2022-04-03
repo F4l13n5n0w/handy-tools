02c810ce9983c36be81f724d43493d7ebdc9e179
F4l13n5n0w <fallensnow.qing@gmail.com>
2022-04-03 15:28:42 +0000

    add mono install

--- auto_codeserver.sh
+++ auto_codeserver.sh
@@ -9,6 +9,13 @@ sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt install net-tools python3 golang tmux -y
 
+## install the latest mono-complete (https://www.mono-project.com/download/stable/#download-lin-ubuntu)
+sudo apt install gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-complete -y
+
 ## install code-server
 cd /opt
 wget https://code-server.dev/install.sh
